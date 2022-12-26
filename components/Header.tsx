753ac650daae449005cc4a6c5399a2d94207d939
Stanislaw Sobczak <sstanislaw85@gmail.com>
2022-12-26 20:38:40 +0100

    Create header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,57 @@
 import Image from "next/image";
 import React from "react";
-import { HomeIcon } from "@heroicons/react/solid";
-import { StarIcon } from "@heroicons/react/outline";
+import {
+  HomeIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+  StarIcon,
+} from "@heroicons/react/outline";
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <StarIcon className="h-7 w-7" />
       </div>
 
-      <div className="">
-        <HomeIcon className="h-60000 w-6" />
-        <p>Strona główna</p>
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Strona główna</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      <form className="flex flex-1 items-center space-x-2 border border-gray-200 bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          name=""
+          id=""
+          placeholder="Wyszukaj na forum"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center lg:hidden">
+        <MenuIcon className="icon" />
       </div>
     </div>
   );
