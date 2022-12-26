2e366aae29f11fc6fdcc28b9041cb10aa34af57a
Stanislaw Sobczak <sstanislaw85@gmail.com>
2022-12-26 19:49:49 +0100

    Fix tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
-
+import { HomeIcon } from "@heroicons/react/solid";
+import { StarIcon } from "@heroicons/react/outline";
 function Header() {
-  return <div>I am the Header</div>;
+  return (
+    <div>
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <StarIcon className="h-7 w-7" />
+      </div>
+
+      <div className="">
+        <HomeIcon className="h-60000 w-6" />
+        <p>Strona główna</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
